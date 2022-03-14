2ad8771b828506b7841a47d1e4e83b2039cbb2da
CalAnizoba <obinnaanizoba5@gmail.com>
2022-03-14 17:34:12 +0800

    Added an event listener to place marker on each cells

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,39 +1,42 @@
-const Container = document.getElementById('grid-container');
-const marker = [];
+const Container = document.querySelector("#grid-container");
+const cells = document.querySelectorAll(".cell");
 
-const playGame = (player, marker) =>{
-    const getName = () => player;
-    const getMarker = () => marker;
-    
+let currentPlayer = 'x';
+let nextPlayer = 'o';
 
-const gameboard = () =>{   
-        for(var i =1; i<10; i++){
-            let box = document.createElement('div');
-            box.classList.add('cell');
-            Container.append(box);
-            box.setAttribute("id", i);
-            
+const gameBoardArray = [];
+
+// const playGame = (player, marker) =>{
+
+//     const getName = () => player;
+//     const getMarker = () => marker;
+//     return {getName, getMarker};
+// };
+
+const currentPlayerMarker = () =>{
+// let currentPlayer = 'o';
+cells.forEach((cell) => {
+    cell.addEventListener('click', (e)=>{
+        let clickedCell = e.target;
+        index = parseInt(clickedCell.getAttribute('id')); //getting id for cell clicked
+        if(gameBoardArray === ''){
+            return; 
+        }else if(clickedCell.textContent !== currentPlayer){
+        gameBoardArray[index] = currentPlayer;
+        clickedCell.textContent = currentPlayer;
+        console.log(gameBoardArray);      
         }
-    };
-    gameboard();
+    });
 
-const placeMarker = () =>{
-        cells.forEach(cell => {
-            cell.addEventListener('click', event=>{
-                index = event.target.id
-               if(index){
-                    cell.textContent = 'x';
-                    marker.splice(index, 1);
-               }
-               
-              
-            });
-        });
-    };
-    return {getName, getMarker, placeMarker};
-}
+});
+};
 
-const player = playGame('Obi', 'x');
 
-marker.push(player);
-        
+
+currentPlayerMarker();
+
+
+// const player1 = playGame('Obi', 'x');
+// const player2 = playGame('ben', 'x');
+
+// console.log(player1);
