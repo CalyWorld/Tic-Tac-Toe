7f23b3b9f91fec61dcbb7e1d89feb39b5ae9c522
CalAnizoba <obinnaanizoba5@gmail.com>
2022-03-16 20:24:09 +0800

    Working on the checkWin Variable function

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,9 +1,19 @@
-const Container = document.querySelector("#grid-container");
-const cells = document.querySelectorAll(".cell");
-let activePlayer = 0;
+const container = document.querySelector("#grid-container");
+const gameBoard = document.querySelectorAll(".cell");
+let activePlayer = true;
 let players = ['x', 'o'];
 
-const gameBoardArray = [];
+const gameBoardArray = ['', '', '', '', '', '', '', '', ''];
+const coordinates = [
+    ['0', '1', '2'],  //0
+    ['3', '4', '5'],  //1
+    ['6', '7', '8'],  //2
+    ['0', '4', '8'],  //3
+    ['2', '4', '6'],  //4
+    ['0', '3', '6'],  //5
+    ['1', '4', '7'],  //6
+    ['2', '5', '8']  //7
+]; 
 
 // const playGame = (player, marker) =>{
 
@@ -12,9 +22,11 @@ const gameBoardArray = [];
 //     return {getName, getMarker};
 // };
 
-const PlaceMarker = () =>{
+const winningMessage = () => console.log(`Player wins`);
+
+const placeMarker = () =>{
 // let currentPlayer = 'o';
-cells.forEach((cell) => {
+    gameBoard.forEach((cell) => {
     cell.addEventListener('click', (e)=>{
         let clickedCell = e.target;
         
@@ -23,7 +35,7 @@ cells.forEach((cell) => {
         if(gameBoardArray === ''){
             return; 
         }
-        else if(clickedCell.textContent !== players){
+        else if(clickedCell.textContent !== players[activePlayer]){
         
             activePlayer = activePlayer == 0 ? 1:0;
             gameBoardArray[index] = players[activePlayer];
@@ -36,13 +48,33 @@ cells.forEach((cell) => {
 });
 };
 
+
+
+
+
 const checkWin = () =>{
-    
-};
+    let roundWon = false;
+    for(let i = 0; i <= 7; i++){
+       let winCondition = coordinates[i]; //stores first array in winCondition
+       let a  = gameBoardArray[winCondition[0]];
+       let b = gameBoardArray[winCondition[1]];
+       let c = gameBoardArray[winCondition[2]];
+       if(a === '' || b === '' || c === ''){
+           continue;
+       }else if(a===b && b===c){
+            roundWon = true;
+            break;
+       }
+    }
+    if(roundWon){
+        winningMessage();
+        activePlayer = false;
+        return;
+    }
+}
 
-PlaceMarker();
-
-
+placeMarker();
+checkWin();
 // const player1 = playGame('Obi', 'x');
 // const player2 = playGame('ben', 'x');
 
